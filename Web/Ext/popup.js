b1504a565ff44fad48120758071925e8184f8670
andrewingor <andrewingor@gmail.com>
2017-11-08 16:33:07 +0200

    Backup

--- Web/Ext/popup.js
+++ Web/Ext/popup.js
@@ -1,9 +1,6 @@
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 /**
  * Get the current URL.
+    "https://api.coinmarketcap.com/v1/ticker/?convert=USD"
  *
  * @param {function(string)} callback called when the URL of the current tab
  *   is found.
